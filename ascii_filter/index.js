9f6cfcca21d16eec9c5ee5de7e7648f1f474cd81
Saif Sohomow <Saif-XI-Coderz@users.noreply.github.com>
2018-10-21 05:13:34 +0400

    ascii_filter

---
+++ ascii_filter/index.js
@@ -0,0 +1,27 @@
+var ctx = canvas.getContext('2d'),
+    img = new Image();
+
+img.onload = draw;
+img.src = "test2.png";
+
+function draw() {
+
+  var dArr = [-1,-1, 0,-1, 1,-1, -1,0, 1,0, -1,1, 0,1, 1,1], // offset array
+      s = 10,  // thickness scale
+      i = 0,  // iterator
+      x = 5,  // final position
+      y = 5;
+  // draw images at offsets from the array scaled by s
+  for(; i < dArr.length; i += 2){
+    ctx.drawImage(img, x + dArr[i]*s, y + dArr[i+1]*s);
+  }
+  
+  // fill with color
+  ctx.globalCompositeOperation = "source-in";
+  ctx.fillStyle = "red";
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+
+  // draw original image in normal mode
+  ctx.globalCompositeOperation = "source-over";
+  ctx.drawImage(img, x, y);
+}
